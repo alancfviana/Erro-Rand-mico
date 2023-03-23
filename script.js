c6a02e7d15e93fe39f1d69789e293daa223ffa1a
Alan <alancfviana@hotmail.com>
2023-03-23 15:30:26 -0300

    Final

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const sorte = () => {sorte
     return new Promise((resolve, reject) => {
         const aleatorio = Math.random()
-        console.log(aleatorio)
+        let verdade = false
         if(aleatorio < 0.5){
-            //console.log("nao")
-        const imagem = "https://tse1.mm.bing.net/th?id=OIP.YGJyDYhXDmKorpYTPiW5EgHaFj&pid=Api&P=0"
-            reject(imagem)
+            verdade = false
         }else{
-            //console.log("sim")
+            verdade = true
+        }
+        console.log(verdade)
+        if(verdade){
+            //console.log("nao")
             const imagem = "https://i.pinimg.com/originals/06/5e/91/065e912c6cfb614a9de0f669de2fb5f2.jpg"
             resolve(imagem)
+        }else{
+            //console.log("sim")
+            const imagem = "https://tse1.mm.bing.net/th?id=OIP.YGJyDYhXDmKorpYTPiW5EgHaFj&pid=Api&P=0"
+            reject(imagem)
         }
     })
 }
